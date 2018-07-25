2856cc3c03734ef81edf321d91fe14a3630164ae
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-07-25 17:09:02 -0400

    Removed unnecessary settings

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,8 @@ import sys, os, docutils
 
 extensions = [
     'sphinx.ext.intersphinx', 
-    'sphinx.ext.doctest'
+    'sphinx.ext.doctest',
+    'sphinx.ext.extlinks'
 ]
 
 source_suffix = '.rst'
@@ -25,29 +26,39 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 
 html_theme_options = {
-    'canonical_url': '',
+    'canonical_url': 'https://xnd.io/',
     'analytics_id': '',
     'logo_only': True,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False,
-    "relbarbgcolor": "black"
+}
+
+html_context = {
+    "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+    "last_updated": True,
+    "commit": False,
+}
+
+html_show_sourcelink = False
+
+extlinks = {
+    'issue': ('https://github.com/plures/xnd/issues/%s', 'GH#'),
+    'pr': ('https://github.com/plures/xnd/pull/%s', 'GH#')
 }
 
 html_logo = "images/xndlogo.png"
 
 
 def setup(app):
-    app.add_crossref_type('topic', 'topic', 'single: %s',
-                          docutils.nodes.strong)
+    app.add_crossref_type(
+        'topic', 'topic', 'single: %s', docutils.nodes.strong
+    )
     app.add_javascript("copybutton.js")
-
 
 
