9fa980a07aea65817b41338fa0fffbe279cc755f
Stefan Krah <skrah@bytereef.org>
2018-07-27 16:45:50 +0200

    Add Windows conda build.

---
+++ scripts/conda-install.bat
@@ -0,0 +1,22 @@
+mkdir build && cd build
+
+git clone https://github.com/plures/ndtypes.git
+git clone https://github.com/plures/xnd.git
+git clone https://github.com/plures/gumath.git
+
+cd ndtypes
+conda build .conda\libndtypes
+conda build .conda\ndtypes
+conda install --use-local --yes ndtypes
+
+cd ..\xnd
+conda build .conda\libxnd
+conda build .conda\xnd
+conda install --use-local --yes xnd
+
+cd ..\gumath
+conda build .conda\libgumath
+conda build .conda\gumath
+conda install --use-local --yes gumath
+
+
