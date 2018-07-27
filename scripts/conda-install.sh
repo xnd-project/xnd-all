2c7b649bd1c5d540e701d6e93df8de2072347ea7
Stefan Krah <skrah@bytereef.org>
2018-07-27 13:56:06 +0200

    Add --yes option to conda install.

--- scripts/conda-install.sh
+++ scripts/conda-install.sh
@@ -14,16 +14,16 @@ git clone https://github.com/plures/gumath.git || exit 1
 cd ndtypes || exit 1
 conda build .conda/libndtypes || exit 1
 conda build .conda/ndtypes || exit 1
-conda install --use-local ndtypes || exit 1
+conda install --use-local --yes ndtypes || exit 1
 
 cd ../xnd || exit 1
 conda build .conda/libxnd || exit 1
 conda build .conda/xnd || exit 1
-conda install --use-local xnd || exit 1
+conda install --use-local --yes xnd || exit 1
 
 cd ../gumath || exit 1
 conda build .conda/libgumath || exit 1
 conda build .conda/gumath || exit 1
-conda install --use-local gumath || exit 1
+conda install --use-local --yes gumath || exit 1
 
 
