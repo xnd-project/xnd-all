916dcac7765beceaf3d38e56d261b642d4a8810a
Stefan Krah <skrah@bytereef.org>
2018-07-31 19:23:58 +0200

    Change output to "true" and "false" for a homogeneous command line.

--- scripts/check_upload.py
+++ scripts/check_upload.py
@@ -4,9 +4,10 @@
 # Check if a conda package with a specific version has already been uploaded to
 # anaconda.org.
 #
-# Return 1 if the package should be uploaded, 0 if it already exists.
+# Write "true" to stdout if the package should be uploaded, "false" if
+# it already exists.
 #
-# Return 1 in case of an error: The upload should be attempted anyway and
+# In case of an error, write "true": The upload should be attempted and
 # fail if the package exists.
 #
 
@@ -16,10 +17,9 @@ import subprocess
 import json
 
 
-
 if len(sys.argv) != 3:
-    sys.stderr.write("usage: check_upload.py: package_name package_path\n\n")
-    sys.exit(1)
+    sys.stdout.write("true\n")
+    sys.exit(0)
 
 package_name = sys.argv[1]
 local_tarname = os.path.basename(sys.argv[2])
@@ -28,14 +28,14 @@ cmd = ["conda", "search", "--json", "-c", "xnd/label/dev", package_name]
 try:
     response = subprocess.check_output(cmd)
 except:
-    sys.stderr.write("check_upload.py: conda search command failed\n\n")
-    sys.exit(1)
+    sys.stdout.write("true\n")
+    sys.exit(0)
 
 try:
     d = json.loads(response)
     remote_tarname = d[package_name][0]["fn"]
 except:
-    sys.stderr.write("check_upload.py: extracting remote tarname failed\n\n")
-    sys.exit(1)
+    sys.stdout.write("true\n")
+    sys.exit(0)
 
-sys.exit(local_tarname != remote_tarname)
+sys.stdout.write("false\n" if local_tarname == remote_tarname else "true\n")
