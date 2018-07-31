c61a1c7b9d55fd6c3fea46e525d12202a9f89723
Stefan Krah <skrah@bytereef.org>
2018-07-31 17:37:19 +0200

    Return 0 if check_upload.py has an internal error.
     - The upload should be attempted in that case.

--- scripts/check_upload.py
+++ scripts/check_upload.py
@@ -4,6 +4,11 @@
 # Check if a conda package with a specific version has already been uploaded to
 # anaconda.org.
 #
+# Return 0 if the package should be uploaded, 1 if it already exists.
+#
+# Return 0 in case of an error: The upload should be attempted anyway and
+# fail if the package exists.
+#
 
 import os
 import sys
@@ -13,7 +18,7 @@ import json
 
 if len(sys.argv) != 3:
     sys.stderr.write("usage: check_upload.py: package_name package_path\n\n")
-    sys.exit(1)
+    sys.exit(0)
 
 package_name = sys.argv[1]
 local_tarname = os.path.basename(sys.argv[2])
@@ -23,8 +28,13 @@ try:
     response = subprocess.check_output(cmd)
 except:
     sys.stderr.write("check_upload.py: conda search command failed\n\n")
-    sys.exit(1)
+    sys.exit(0)
 
-d = json.loads(response)
-remote_tarname = d[package_name][0]["fn"]
+try:
+    d = json.loads(response)
+    remote_tarname = d[package_name][0]["fn"]
+except:
+    sys.stderr.write("check_upload.py: extracting remote tarname failed\n\n")
+    sys.exit(0)
+
 sys.exit(local_tarname == remote_tarname)
