d14e79d7e49f99af94239faee48580494ea3c928
Stefan Krah <skrah@bytereef.org>
2018-07-31 18:36:54 +0200

    Fix shell syntax and use "1" for "true" in check_upload.py.

--- scripts/check_upload.py
+++ scripts/check_upload.py
@@ -4,9 +4,9 @@
 # Check if a conda package with a specific version has already been uploaded to
 # anaconda.org.
 #
-# Return 0 if the package should be uploaded, 1 if it already exists.
+# Return 1 if the package should be uploaded, 0 if it already exists.
 #
-# Return 0 in case of an error: The upload should be attempted anyway and
+# Return 1 in case of an error: The upload should be attempted anyway and
 # fail if the package exists.
 #
 
@@ -16,9 +16,10 @@ import subprocess
 import json
 
 
+
 if len(sys.argv) != 3:
     sys.stderr.write("usage: check_upload.py: package_name package_path\n\n")
-    sys.exit(0)
+    sys.exit(1)
 
 package_name = sys.argv[1]
 local_tarname = os.path.basename(sys.argv[2])
@@ -28,13 +29,13 @@ try:
     response = subprocess.check_output(cmd)
 except:
     sys.stderr.write("check_upload.py: conda search command failed\n\n")
-    sys.exit(0)
+    sys.exit(1)
 
 try:
     d = json.loads(response)
     remote_tarname = d[package_name][0]["fn"]
 except:
     sys.stderr.write("check_upload.py: extracting remote tarname failed\n\n")
-    sys.exit(0)
+    sys.exit(1)
 
-sys.exit(local_tarname == remote_tarname)
+sys.exit(local_tarname != remote_tarname)
